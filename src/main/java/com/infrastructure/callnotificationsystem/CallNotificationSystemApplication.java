453516c81473ad54df2c3bab9c2caf6f4ecffac5
sahiniscimen <sahiniscimen@gmail.com>
2021-01-14 21:07:55 +0300

    initial commit

---
+++ src/main/java/com/infrastructure/callnotificationsystem/CallNotificationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.infrastructure.callnotificationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallNotificationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallNotificationSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infrastructure/callnotificationsystem/CallNotificationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infrastructure.callnotificationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CallNotificationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
